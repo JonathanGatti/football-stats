7cc9e2d58107e9954221e843bcd47933c3d9a22b
jonathan gatti <jonathangatti2@gmail.com>
2020-11-14 14:28:33 +0000

    parsed data in array of matches

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import { match } from 'assert';
 import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+});
+
+const array = matches.split('\n').map(match => match.split(','))
+
