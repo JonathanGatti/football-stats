c0a5a1bd41123d9308cd1cd3e525a5c0c7e54f44
jonathan gatti <jonathangatti2@gmail.com>
2020-11-14 15:27:23 +0000

    created csv file reader

--- build/index.js
+++ build/index.js
@@ -1,10 +1,23 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
-var matches = fs_1.default.readFileSync('football.csv', {
-    encoding: 'utf-8'
-});
-var array = matches.split('\n').map(function (match) { return match.split(','); });
+var CsvFileReader_1 = require("./CsvFileReader");
+var matches = new CsvFileReader_1.CsvFileReader('football.csv');
+matches.read();
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var calculateWins = function (teamName, matches) {
+    var wins = 0;
+    for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+        var match = matches_1[_i];
+        if (match[1] === teamName && match[5] === MatchResult.HomeWin)
+            wins++;
+        else if (match[2] === teamName && match[5] === MatchResult.AwayWin)
+            wins++;
+    }
+    console.log(teamName + " wins: " + wins);
+};
+calculateWins('Cardiff', matches.data);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import { match } from 'assert';
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8'
-});
+const matches = new CsvFileReader('football.csv');
+matches.read();
 
-const array = matches.split('\n').map(match => match.split(','))
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+const calculateWins = (teamName: string, matches: string[][]): void => {
+  let wins = 0;
+  for (let match of matches) {
+    if (match[1] === teamName && match[5] === MatchResult.HomeWin) wins++;
+    else if (match[2] === teamName && match[5] === MatchResult.AwayWin) wins ++;
+  }
+  console.log(`${teamName} wins: ${wins}`)
+}
+
+calculateWins('Cardiff', matches.data)
 
